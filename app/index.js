abaffdc8c385bb98cf7cc7928bee62eca5055d43
Juscelino Junior <juscelino.junior@dremio.com>
2022-10-18 19:34:25 -0300

    feat: change list to be a list by seller

--- app/index.js
+++ app/index.js
@@ -16,7 +16,6 @@ function uploadFile() {
 
   const xhttp = new XMLHttpRequest();
   xhttp.open("POST", "http://127.0.0.1:5000/upload_sales");
-  // xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   xhttp.send(formData);
   xhttp.onreadystatechange = function() {
     if (this.readyState === 4) {
@@ -37,23 +36,67 @@ function loadTable() {
   xhttp_owners.open("GET", "http://127.0.0.1:5000/sales");
   xhttp_owners.send();
   xhttp_owners.onreadystatechange = function() {
+    let tableHTML;
+    let sales_table_identifier;
+    let node;
     if (this.readyState === 4 && this.status === 200) {
       console.log(this.responseText);
-      let trHTML = '';
-      const objects = JSON.parse(this.responseText);
-      for (let object of objects) {
-        let sale_date = new Date(object['date'])
+      const sales = JSON.parse(this.responseText);
+      i = 0
+      let sales_list;
+      for ([seller, sales_list] of Object.entries(sales)) {
 
-        trHTML += '<tr>';
-        trHTML += '<td>' + object['id'] + '</td>';
-        trHTML += '<td>' + object['product'] + '</td>';
-        trHTML += '<td>' + object['seller'] + '</td>';
-        trHTML += '<td>' + object['type'] + '</td>';
-        trHTML += '<td>' + object['value'] + '</td>';
-        trHTML += '<td>' + (sale_date.getDay()+1).toString().padStart(2, "0") + "/" + (sale_date.getMonth()+1).toString().padStart(2, "0") + "/" + sale_date.getFullYear() + '</td>';
-        trHTML += "</tr>";
+        sales_table_identifier = "sales_table" + i
+
+        tableHTML = "<div class=\"table-responsive\">\n" +
+            `        <h3>${seller}</h3>` +
+            "        <table class=\"table\">\n" +
+            "          <thead>\n" +
+            "            <tr>\n" +
+            "              <th scope=\"col\">#</th>\n" +
+            "              <th scope=\"col\">Product</th>\n" +
+            "              <th scope=\"col\">Type</th>\n" +
+            "              <th scope=\"col\">Value</th>\n" +
+            "              <th scope=\"col\">Date</th>\n" +
+            "            </tr>\n" +
+            "          </thead>\n" +
+            `          <tbody id=${sales_table_identifier}>\n` +
+            "            <tr>\n" +
+            "              <th scope=\"row\" colspan=\"5\">Loading...</th>\n" +
+            "            </tr>\n" +
+            "          </tbody>\n" +
+            "        </table>\n" +
+            "      </div>"
+
+        node = document.createRange().createContextualFragment(tableHTML);
+        document.getElementById("list").appendChild(node);
+
+        let trHTML = '';
+        let sum_value = 0;
+
+        sales_list.forEach(function (item) {
+          let sale_date = new Date(item.date)
+
+          if (item.type === "Comissão paga") {
+            sum_value -= item.value
+          } else {
+            sum_value += item.value
+          }
+
+          trHTML += '<tr>';
+          trHTML += '<td>' + item.id + '</td>';
+          trHTML += '<td>' + item.product + '</td>';
+          trHTML += '<td>' + item.type + '</td>';
+          trHTML += '<td>' + item.value + '</td>';
+          trHTML += '<td>' + (sale_date.getDay() + 1).toString().padStart(2, "0") + "/" + (sale_date.getMonth() + 1).toString().padStart(2, "0") + "/" + sale_date.getFullYear() + '</td>';
+          trHTML += "</tr>";
+        })
+
+        i++
+
+        trHTML += `<h5>Total value: ${sum_value}</h5>`
+        document.getElementById(sales_table_identifier).innerHTML = trHTML;
       }
-      document.getElementById("sales_table").innerHTML = trHTML;
     }
   };
 }
